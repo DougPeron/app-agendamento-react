c1dc6a91f03ccaa0ce6eb2c96a009485871fabed
Douglas Peron <peron930@gmail.com>
2025-06-09 17:03:30 -0300

    Update
    Criação pagina de Login, estrutura e estilização.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
+import LoginPage from './Components/Login/LoginPage'
+import Dashboard from './Components/Dashboard/Dashboard';
+import PainelUsuarioPage from './Components/PainelUsuario/PainelUsuarioPage';
 
 function App() {
 
   return (
     <>
-      
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard-adm" element={<Dashboard />} />
+        <Route path="/painel-usuario" element={<PainelUsuarioPage />} />
+      </Routes>
+    </Router>
     </>
   )
 }
--- src/Components/Login/LoginPage.jsx
+++ src/Components/Login/LoginPage.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
+import { useState } from 'react';
+import './LoginPage.scss'
 
 const LoginPage = () => {
+    const [username, setUsername] = useState("");
+    const [userpassword, setPassword] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(username == 'admin@email.com' && userpassword == 'admin'){
+            alert('logado');
+        }else{
+            alert('Credenciais inválidas!');
+        }   
+    };
   return (
-    <div>
-      
+    <div className='login-container'>
+        <div className='login-card'>
+            <div className='form-section'>
+                <h1 className='title'>Seja bem-vindo</h1>
+                <p className='subtitle'>Entre com sua conta para acessar o painel</p>
+                <form onSubmit={handleSubmit} className='form' htmlFor='formulario'>   
+                    <div className='input-group'>
+                        <label htmlFor="email" className="label" id=''>E-mail</label>
+                        <input className='input' type="email" placeholder="admin@email.com" onChange={(e) => setUsername(e.target.value)} />
+                    </div>
+                    <div className='input-group'>
+                        <label htmlFor="password" className='label' id=''>Senha</label>
+                        <input className='input' type="password" placeholder="admin" onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <button type='submit' className='submit-button'>Entrar</button>
+                </form>
+            </div>
+        </div>
     </div>
   )
 }
 
-export default LoginPage
+export default LoginPage;
